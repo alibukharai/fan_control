e8b37d4e2010d1f530b52dddafbf62a13922073b
Ali Hassan Shah <ali.shah@espressif.com>
2023-05-26 19:10:02 +0800

    Data collection

--- main/main.c
+++ main/main.c
@@ -6,7 +6,9 @@
 #include "esp_err.h"
 #include "freertos/semphr.h"
 #include "driver/adc.h"
-#include "esp_adc_cal.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 
 
 #define LEDC_TIMER              LEDC_TIMER_0
@@ -14,59 +16,58 @@
 #define LEDC_OUTPUT_IO          (4) // Define the output GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
 #define LEDC_DUTY_RES           LEDC_TIMER_10_BIT // Set duty resolution to 10 bits ((2 ** 10)-1)
-#define LEDC_DUTY               (716) // Set duty to 50%. (300/LEDC_TIMER_10_BIT) * 100% = 4095
+#define LEDC_DUTY               (1000) //716 Set duty to 70%. (?/LEDC_TIMER_10_BIT) * 100% = 716
 #define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
 #define GPIO_INTERRUPT_PIN      (5)
 
 #define ADC_UNIT                ADC_UNIT_1
-#define ADC_CHANNEL             ADC_CHANNEL_2//ADC_CHANNEL_6
-#define DEFAULT_VREF            3300    // Default ADC reference voltage (in mV)
-#define NO_OF_SAMPLES           64      // Number of samples to take for ADC averaging
+#define ADC_CHANNEL             ADC_CHANNEL_2
+#define ADC_ATTEN             ADC_ATTEN_DB_11 // ADC_BITWIDTH_DEFAULT //  ADC_ATTEN_DB_11
 
-int counter = 0;
+uint16_t counter = 0;
 SemaphoreHandle_t counterSemaphore;
-esp_adc_cal_characteristics_t adc_calib;
-
+adc_oneshot_unit_handle_t adc_handle;
 
 void adc_read_task(void *args)
 {
     while (1) {
-        uint32_t adc_reading = 0;
-        
-        // Take multiple ADC samples and average the results
-        for (int i = 0; i < NO_OF_SAMPLES; i++) {
-            adc_reading += adc1_get_raw(ADC_CHANNEL);
-        }
-        adc_reading /= NO_OF_SAMPLES;
-
-        // Convert ADC reading to voltage in mV
-        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, &adc_calib);
-        printf("ADC Channel %d: Raw value: %d\tVoltage: %dmV\n", ADC_CHANNEL, adc_reading, voltage);
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+        int raw;
+        ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL, &raw));
+        printf("ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL, raw);
+        vTaskDelay(pdMS_TO_TICKS(3000));
     }
+}
+
+void IRAM_ATTR intr_handler(void *args)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xSemaphoreGiveFromISR(counterSemaphore, &xHigherPriorityTaskWoken);
+    counter++;
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 void countTask(void *args)
 {
-    TickType_t lastWakeTime = xTaskGetTickCount();
     while (1) {
-        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000)); // Delay for 1 second
+        vTaskDelay(pdMS_TO_TICKS(3000)); // Delay for 1 second
         xSemaphoreTake(counterSemaphore, portMAX_DELAY);
         printf("Current RPM = %d\n", counter * 30);
         counter = 0;
     }
 }
 
-void adc_init(){
-        // Configure ADC
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_0); //ADC_ATTEN_DB_11
-
-    adc1_config_width(ADC_WIDTH_BIT_12);               // Configure ADC to 12-bit width
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_0);  // Set attenuation for the ADC channel
-
-    // Calibrate ADC based on measured reference voltage
-    esp_adc_cal_characterize(ADC_UNIT, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, DEFAULT_VREF, &adc_calib);
+void adc_init()
+{
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc_handle));
+    //-------------ADC1 Config---------------//
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT, //ADC_WIDTH_BIT_12
+        .atten = ADC_ATTEN,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL, &config));
 }
 
 static void example_ledc_init(void)
@@ -76,7 +77,7 @@ static void example_ledc_init(void)
         .speed_mode = LEDC_MODE,
         .duty_resolution = LEDC_DUTY_RES,
         .timer_num = LEDC_TIMER,
-        .freq_hz = LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+        .freq_hz = LEDC_FREQUENCY,  // Set output frequency at 25 kHz
         .clk_cfg = LEDC_AUTO_CLK
     };
     ESP_ERROR_CHECK(ledc_timer_config(&timer));
@@ -101,14 +102,6 @@ static void example_ledc_init(void)
     ESP_ERROR_CHECK(gpio_config(&io_conf));
 }
 
-static void IRAM_ATTR intr_handler(void *args)
-{
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    xSemaphoreGiveFromISR(counterSemaphore, &xHigherPriorityTaskWoken);
-    counter++;
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-}
-
 void app_main(void)
 {
     adc_init();
@@ -125,5 +118,4 @@ void app_main(void)
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(1000)); // Delay to keep the tasks running
     }
-
 }
